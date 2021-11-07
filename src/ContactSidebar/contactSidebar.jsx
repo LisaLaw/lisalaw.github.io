703ed02a96a313d224c191b1af7f391d1a5a683b
suqui <lisacatlaw@gmail.com>
2021-11-07 11:02:37 +0100

    styles for contact sidebar

--- src/ContactSidebar/contactSidebar.jsx
+++ src/ContactSidebar/contactSidebar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import email from '../logos/email.svg';
+import email from '../logos/email.png';
 import telephone from '../logos/telephone.svg';
-import { StyledContactSidebar, StyledContactLink, Icon } from './styles';
+import { StyledContactSidebar, StyledContactLink, Icon, Separator } from './styles';
 
 export const ContactSidebar = () => {
     return <StyledContactSidebar>
-        <StyledContactLink href='mailto:lisacatlaw@gmail.com'>
+        <StyledContactLink href='mailto:lisacatlaw@gmail.com' alt='Write to me'>
             <Icon src={email} />
         </StyledContactLink>
-        <StyledContactLink href='tel:639-624-915'>
+        <Separator />
+        <StyledContactLink href='tel:639-624-915' alt='Call me'>
             <Icon src={telephone} />
         </StyledContactLink>
     </StyledContactSidebar>
--- src/ContactSidebar/styles.js
+++ src/ContactSidebar/styles.js
@@ -1,9 +1,65 @@
 import styled from 'styled-components';
+import { screenSize, colors } from '../constants/constants';
+import logo from '../logos/logo_left.svg';
 
-const StyledContactSidebar = styled.div``;
+const StyledContactSidebar = styled.div`
+    display: flex;
+    display: row;
+    align-items: center;
+    align-self: center;
 
-const StyledContactLink = styled.a``;
+    @media (min-width: ${screenSize.mobile}) {
+        flex-direction: column;
+        align-self: flex-start;
+        border: 0.2px solid ${colors.grayColor.dark};
+        border-left: none;
+        padding-left: 2px;
+        position: absolute;
+        top: 35%;
+        border-radius: 0 3px 3px 0;
+        background-image: url(${logo});
+        background-position: left;
+        background-repeat: no-repeat;
+        background-size: fit;
+        height: 40px;
+        width: 20px;
 
-const Icon = styled.img``;
+        :hover {
+            background: none;
+            height: unset;
+            width: unset;
+            padding: unset;
+        }
 
-export { StyledContactSidebar, StyledContactLink, Icon };
+        :hover a {
+            transition: 0.7s all ease-in-out;
+            display: flex;
+        }
+    }
+`;
+
+const StyledContactLink = styled.a`
+    @media (min-width: ${screenSize.mobile}) {
+        display: none;
+    }
+`;
+
+const Icon = styled.img`
+    width: 32px;
+    margin: 15px 20px;
+
+    @media (min-width: ${screenSize.mobile}) {
+        margin: 12px 10px;
+        width: 32px;
+    }
+`;
+
+const Separator = styled.div`
+    border: 0.2px solid ${colors.grayColor.dark};
+    height: 32px;
+    @media (min-width: ${screenSize.mobile}) {
+        display: none;
+    }
+`;
+
+export { StyledContactSidebar, StyledContactLink, Icon, Separator };
